54510b677ffe264461b03d681261b4e172459b6b
Ezeiac <ezearena@gmail.com>
2026-06-13 04:35:37 +0200

    cambios animacion

--- src/scripts/features/animation.ts
+++ src/scripts/features/animation.ts
@@ -1,17 +1,10 @@
-interface FontStyle {
-    font: string;
-    weight: string;
-    size: string;
-    color: string;
-}
-
-import { getNodes, mediaQ } from "../nodes";
-
-const nodes = getNodes()
-const title = nodes.title
-const subtitle = nodes.subtitle
+import { getNodes } from "../nodes";
 
 document.addEventListener("DOMContentLoaded", () => {
+    const nodes = getNodes()
+    const title = nodes.title
+    const subtitle = nodes.subtitle
+    const againNo = localStorage.getItem('intro')
 
     const letters = "abcdefghijklmnopqrstuvwxyz";
     const h1Text = "Ezequiel Arena";
@@ -20,8 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const h1Array = h1Text.split("");
     const h2Array = h2Text.split("");
 
-    let showAll = false;
-
     const generateText = (
         container: HTMLElement,
         text: Array<string>,
@@ -29,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
         maxCambios: number = 40,
         delay: number,
         isTrue: boolean) => {
+
         let indexLastLetter = 0
         const totalLetras = text.length;
 
@@ -54,7 +46,9 @@ document.addEventListener("DOMContentLoaded", () => {
                         newP.textContent = text[index] ?? " ";
                         indexLastLetter++;
                         if (indexLastLetter === totalLetras && isTrue) {
-                            showSite()
+                            setTimeout(() => {
+                                showSite()
+                            }, 300);
                         }
                         return;
                     }
@@ -70,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         timeToChange = timeToChange * 0.85;
                         if (timeToChange < 80) timeToChange = 80;
                     } else {
-                        timeToChange = timeToChange * 1.18;
+                        timeToChange = timeToChange * 1.15;
                     }
                     setTimeout(ejecutarCambio, timeToChange);
                 }
@@ -88,20 +82,31 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!nodes.hidden) return
         nodes.hidden.classList.remove("hidden");
         setTimeout(() => {
-            if (nodes.hidden) nodes.hidden.classList.remove("opacity-0", "translate-y-4");
-            if (nodes.aboutMe) nodes.aboutMe.classList.remove("opacity-0", "translate-y-4");
+            nodes.hidden?.classList.remove("opacity-0", "translate-y-4");
+            nodes.aboutMe?.classList.remove("opacity-0", "translate-y-4");
 
-            if (nodes.menu) {
-                nodes.menu.classList.remove("opacity-0", "translate-y-full", "md:-translate-x-full");
+            title?.classList.remove('opacity-0')
+            subtitle?.classList.remove('opacity-0')
+            nodes.menu?.classList.remove("opacity-0", "translate-y-full", "md:-translate-x-full");
+            nodes.menu?.classList.add("opacity-100", "translate-y-0", "md:translate-x-0");
 
-                nodes.menu.classList.add("opacity-100", "translate-y-0", "md:translate-x-0");
-            }
-        }, 500);
+        }, 50);
     }
 
+    localStorage.setItem('intro', 'true')
+
     if (!title || !subtitle) return
-    generateText(title, h1Array, 10, 30, 0, false)
-    generateText(subtitle, h2Array, 15, 40, 300, true)
+
+    if (!againNo) {
+        generateText(title, h1Array, 10, 30, 0, false)
+        generateText(subtitle, h2Array, 15, 40, 300, true)
+    } else {
+        title.innerText = h1Text
+        subtitle.innerText = h2Text
+        title.classList.add('noAnimated')
+        subtitle.classList.add('noAnimated')
+        showSite()
+    }
 });
 
 
